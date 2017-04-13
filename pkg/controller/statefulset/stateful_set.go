3f2c96bbc283e63dba34adbcf6a361f5b6a462d5
xu fei <1004815462@qq.com>
2017-04-13 10:55:47 +0800

    Update stateful_set.go

--- pkg/controller/statefulset/stateful_set.go
+++ pkg/controller/statefulset/stateful_set.go
@@ -85,7 +85,7 @@ func NewStatefulSetController(
 	eventBroadcaster := record.NewBroadcaster()
 	eventBroadcaster.StartLogging(glog.Infof)
 	eventBroadcaster.StartRecordingToSink(&v1core.EventSinkImpl{Interface: v1core.New(kubeClient.Core().RESTClient()).Events("")})
-	recorder := eventBroadcaster.NewRecorder(api.Scheme, clientv1.EventSource{Component: "statefulset"})
+	recorder := eventBroadcaster.NewRecorder(api.Scheme, clientv1.EventSource{Component: "statefulset-controller"})
 
 	ssc := &StatefulSetController{
 		kubeClient: kubeClient,
